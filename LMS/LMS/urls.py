6331cd20e1cc3ef5f8b48d1ed775c3672d07a9db
Hammad Siddiqi (2211-009-KHI-DEG) <hammad.siddiqi@xloopdigital.com>
2023-08-17 13:44:40 +0500

    URLS corrected

--- LMS/LMS/urls.py
+++ LMS/LMS/urls.py
@@ -20,8 +20,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('accounts.urls')),
-    path('', include('course.urls'))
+    path('', include('accounts.urls')),
+    path('api/', include('course.urls'))
 ]
 from django.conf import settings
 from django.conf.urls.static import static
